bfd30b322d0364fa938befe8ce962bfc537c742d
Julian Coleman <jcoleman.engineering@gmail.com>
2026-07-02 17:38:16 -0600

    Clock: split JP chars vertically, workspace: remove icons

--- waybar/.config/waybar/scripts/time_jp.sh
+++ waybar/.config/waybar/scripts/time_jp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Clock with Japanese AM/PM + calendar tooltip
+# Clock with Japanese AM/PM (vertically split) + calendar tooltip
 ampm=$(date +%p)
 if [[ "$ampm" == "AM" ]]; then
     jpampm="午前"
@@ -9,14 +9,14 @@ fi
 hour=$(date +%I)
 min=$(date +%M)
 
-# Vertical: JP AM/PM, hour, min on separate lines
-vertical=$(printf "%s\n%s\n%s" "$jpampm" "$hour" "$min")
+# Split JP chars vertically using grep -o (UTF-8 safe)
+jp_vertical=$(printf '%s' "$jpampm" | grep -o . | paste -sd'\n')
+vertical=$(printf "%s\n%s\n%s" "$jp_vertical" "$hour" "$min")
 
 # Calendar
 calendar=$(cal)
 
 # Build JSON manually with proper UTF-8
-# Escape newlines and quotes for JSON
 text_escaped=$(printf '%s' "$vertical" | sed 's/\\/\\\\/g; s/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 cal_escaped=$(printf '%s' "$calendar" | sed 's/\\/\\\\/g; s/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
