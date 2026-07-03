af46c0f356e3b50c2fc6a09425863a49fd05ada4
Julian Coleman <jcoleman.engineering@gmail.com>
2026-07-02 19:07:15 -0600

    Add GPU fan speed to fans tooltip

--- waybar/.config/waybar/scripts/fans.sh
+++ waybar/.config/waybar/scripts/fans.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# Fan speeds from NCT6797D
+# Fan speeds from NCT6797D + GPU fan
 tooltip=""
+
+# Chassis fans from NCT6797D
 for i in 1 2 3 4 5 6; do
     rpm=$(cat /sys/class/hwmon/hwmon3/fan${i}_input 2>/dev/null)
     [ -z "$rpm" ] && continue
@@ -10,6 +12,12 @@ for i in 1 2 3 4 5 6; do
         tooltip="${tooltip}Fan ${i}: ${rpm} RPM\n"
     fi
 done
+
+# GPU fan speed (percentage)
+gpu_fan=$(nvidia-smi --query-gpu=fan.speed --format=csv,noheader,nounits 2>/dev/null)
+if [ -n "$gpu_fan" ]; then
+    tooltip="${tooltip}GPU: ${gpu_fan}%\n"
+fi
 
 # Trim trailing newline
 tooltip=$(echo -e "$tooltip" | sed '/^$/d')
